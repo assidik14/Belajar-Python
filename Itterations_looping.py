18952d0cf6b8eddc0437e932b4927d60181078d8
Achmad Maulana Assidik <66199955+assidik14@users.noreply.github.com>
2024-07-01 22:26:29 +0700

    Create Itterations_looping.py

---
+++ Itterations_looping.py
@@ -0,0 +1,18 @@
+print("===== FOR LOOP =====")
+# FOR LOOP
+for i in range(5):
+    print("Hello World!")
+
+print("==== WHILE LOOP ====")
+# WHILE LOOP
+i = 0
+while i < 10:
+    print("Increment looping")
+    i = i + 1 # Increment
+
+print("===================")
+
+i = 10
+while i > 0:
+    print("Decrement looping")
+    i = i - 1 # Decrement
