41fe8b8c8f86fdc91ae268fbe04a73815a22b23e
Achmad Maulana Assidik <achmad.assidik@brilife.co.id>
2024-07-06 01:26:12 +0700

    Belajar Python

--- operator-logika.py
+++ operator-logika.py
@@ -1,11 +1,17 @@
-print("Operator logika AND")
+print("Operator logika AND") # Akan True jika kedua nilai True
 print(True and True)
 print(True and False)
 print(False and True)
 print(False and False)
 print("--------------------")
-print("Operator logika OR")
+print("Operator logika OR") # Akan Trus jika salah satu atau kedua-nya True
 print(True or True)
 print(True or False)
 print(False or True)
 print(False or False)
+print("--------------------")
+print("Operator logika XOR") # Akan True jika salah satu-nya True
+print(True ^ True)
+print(True ^ False)
+print(False ^ True)
+print(False ^ False)
