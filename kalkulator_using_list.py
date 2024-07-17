7505fa3fd409996e074c09132bca69f11266ba82
assidik14 <a.maulanaassidik14@gmail.com>
2024-07-17 21:46:10 +0700

    Belajar Python

--- kalkulator_using_list.py
+++ kalkulator_using_list.py
@@ -1,12 +1,12 @@
 angka = []
 
-user_input1 = int(input("Masukkan angka                     : "))
-user_input2 = input("Masukkan operator (+ | - | : | x)  : ")
-user_input3 = int(input("Masukkan angka                     : "))
+# user_input1 = int(input("Masukkan angka                     : "))
+# user_input2 = input("Masukkan operator (+ | - | : | x)  : ")
+# user_input3 = int(input("Masukkan angka                     : "))
 
-angka.append(user_input1)
-angka.append(user_input2)
-angka.append(user_input3)
+angka.append(int(input("Masukkan angka                     : ")))
+angka.append(input("Masukkan operator (+ | - | : | x)  : "))
+angka.append(int(input("Masukkan angka                     : ")))
 
 match angka[1]:
     case "+" : hasil = angka[0] + angka[2]
